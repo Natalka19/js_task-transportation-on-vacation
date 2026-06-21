fc768daabe04b333ab929123f63e9ee8ab36c343
Natalka Tkhoriv <natalka.tkhoriv19@gmail.com>
2026-06-21 07:59:16 +0200

    complete task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const dailyRate = 40;
+  const total = days * dailyRate;
+
+  if (days >= 7) {
+    return total - 50;
+  }
+
+  if (days >= 3) {
+    return total - 20;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
