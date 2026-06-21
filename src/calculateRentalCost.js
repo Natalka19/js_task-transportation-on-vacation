40b837ec1ad0ab191443594cd2b2e01bea5df662
Natalka Tkhoriv <natalka.tkhoriv19@gmail.com>
2026-06-21 21:10:44 +0200

    add const

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,16 +3,20 @@
  *
  * @return {number}
  */
+
+const DAILY_RATE = 40;
+const LONG_TERM_RENTAL_DAYS = 7;
+const LONG_TERM_DISCOUNT = 50;
+const MID_TERM_RENTAL_DAYS = 3;
+const MID_TERM_DISCOUNT = 20;
+
 function calculateRentalCost(days) {
-  const dailyRate = 40;
-  const total = days * dailyRate;
+  let total = days * DAILY_RATE;
 
-  if (days >= 7) {
-    return total - 50;
-  }
-
-  if (days >= 3) {
-    return total - 20;
+  if (days >= LONG_TERM_RENTAL_DAYS) {
+    total -= LONG_TERM_DISCOUNT;
+  } else if (days >= MID_TERM_RENTAL_DAYS) {
+    total -= MID_TERM_DISCOUNT;
   }
 
   return total;
